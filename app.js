87695dcea74ad7366fb31ec41355bd670a52a4f7
Raktimabho Ghosh <raktimabhoghosh@Raktimabhos-MacBook-Air.local>
2025-08-03 22:43:27 +0530

    Project setup

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.listen(port, (err) => {
     if (err) {
         return console.log('Something bad happened', err);
     }
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on ${port}...`);
 });
 
 
