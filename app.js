00604e8878e2fd9c644c90d1d999a7043d14d042
Raktimabho Ghosh <raktimabhoghosh@Raktimabhos-MacBook-Air.local>
2025-08-29 21:33:26 +0530

    adding a newline at the end of the file to follow POSIX standards 2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
+// const port = process.env.PORT;
 const taskRouter = require('./routes/tasks.route');
 
 
